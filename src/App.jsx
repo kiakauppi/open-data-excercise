4277d49d9cb8c7533fa801c148eec4ebd0f83b61
kiakauppi <n2kaki00@students.oamk.fi>
2024-10-27 20:40:24 +0200

    Added a button to the page and some styling for it

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import './App.css';
 function App() {
   const [catImages, setCatImages] = useState([]);
 
-  useEffect(() => {
+  const fetchCatImages = () => {
     fetch('https://api.thecatapi.com/v1/images/search?limit=10')
       .then(response => response.json())
       .then(data => setCatImages(data));
+  };
+
+  useEffect(() => {
+    fetchCatImages();
   }, []);
 
   return (
     <div>
       <h3>Cat Gallery</h3>
+      <button onClick={fetchCatImages}>Generate New Cats</button>
       <div>
         {catImages.map((cat, index) => (
           <img key={index} src={cat.url} alt="A cute cat" width="200" height="200" />
